1fcbfb8d3e07fa2a5d54bfa1f8a29702e70a76fa
phongpham2909 <phongpham2140051@gmail.com>
2025-03-03 01:07:38 +0700

    update: route path images

--- variables/images.ts
+++ variables/images.ts
@@ -1,22 +1,22 @@
 // ----------------- AVATAR ----------------- //
-const HLC_AVATAR_0 = '/assets/avatar/hlc-avatar-0.png';
-const HLC_AVATAR_1 = '/assets/avatar/hlc-avatar-1.png';
-const HLC_AVATAR_2 = '/assets/avatar/hlc-avatar-2.png';
-const HLC_AVATAR_3 = '/assets/avatar/hlc-avatar-3.png';
-const HLC_AVATAR_4 = '/assets/avatar/hlc-avatar-4.png';
-const HLC_AVATAR_5 = '/assets/avatar/hlc-avatar-5.png';
-const HLC_AVATAR_6 = '/assets/avatar/hlc-avatar-6.png';
-const HLC_AVATAR_7 = '/assets/avatar/hlc-avatar-7.png';
-const HLC_AVATAR_8 = '/assets/avatar/hlc-avatar-8.png';
-const HLC_AVATAR_9 = '/assets/avatar/hlc-avatar-9.png';
-const HLC_AVATAR_10 = '/assets/avatar/hlc-avatar-10.png';
-const HLC_AVATAR_11 = '/assets/avatar/hlc-avatar-11.png';
-const HLC_AVATAR_12 = '/assets/avatar/hlc-avatar-12.png';
-const HLC_AVATAR_13 = '/assets/avatar/hlc-avatar-13.png';
-const HLC_AVATAR_14 = '/assets/avatar/hlc-avatar-14.png';
-const HLC_AVATAR_15 = '/assets/avatar/hlc-avatar-15.png';
-const HLC_AVATAR_BADGE = '/assets/avatar/hlc-avatar-badge.svg';
-const HLC_AVATAR_LOGO = '/assets/avatar/hlc-avatar-logo.png';
+const HLC_AVATAR_0 = '/assets/images/avatar/hlc-avatar-0.png';
+const HLC_AVATAR_1 = '/assets/images/avatar/hlc-avatar-1.png';
+const HLC_AVATAR_2 = '/assets/images/avatar/hlc-avatar-2.png';
+const HLC_AVATAR_3 = '/assets/images/avatar/hlc-avatar-3.png';
+const HLC_AVATAR_4 = '/assets/images/avatar/hlc-avatar-4.png';
+const HLC_AVATAR_5 = '/assets/images/avatar/hlc-avatar-5.png';
+const HLC_AVATAR_6 = '/assets/images/avatar/hlc-avatar-6.png';
+const HLC_AVATAR_7 = '/assets/images/avatar/hlc-avatar-7.png';
+const HLC_AVATAR_8 = '/assets/images/avatar/hlc-avatar-8.png';
+const HLC_AVATAR_9 = '/assets/images/avatar/hlc-avatar-9.png';
+const HLC_AVATAR_10 = '/assets/images/avatar/hlc-avatar-10.png';
+const HLC_AVATAR_11 = '/assets/images/avatar/hlc-avatar-11.png';
+const HLC_AVATAR_12 = '/assets/images/avatar/hlc-avatar-12.png';
+const HLC_AVATAR_13 = '/assets/images/avatar/hlc-avatar-13.png';
+const HLC_AVATAR_14 = '/assets/images/avatar/hlc-avatar-14.png';
+const HLC_AVATAR_15 = '/assets/images/avatar/hlc-avatar-15.png';
+const HLC_AVATAR_BADGE = '/assets/images/avatar/hlc-avatar-badge.svg';
+const HLC_AVATAR_LOGO = '/assets/images/avatar/hlc-avatar-logo.png';
 
 export const images = {
   // ----------------- AVATAR ----------------- //
