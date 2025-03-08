052257fd00e10f85cb845919ec3aafc0dd202f7f
phongpham2909 <phongpham2140051@gmail.com>
2025-03-09 00:08:15 +0700

    update: slider component

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,5 +1,85 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import clsx from 'clsx';
+import * as RadixUISlider from '@radix-ui/react-slider';
 
-export const Slider = () => {
-  return <div>Slider</div>;
+import { PREFIX_CLASS } from '@/variables/app';
+
+import './slider.css';
+
+type SliderTooltipPlacement = 'top' | 'bottom';
+
+interface SliderTooltip {
+  placement?: SliderTooltipPlacement;
+  formatter?: (value: number) => React.ReactNode | null;
+  formatterLabel?: (value: number) => React.ReactNode | null;
+}
+
+export interface SliderProps extends Omit<RadixUISlider.SliderProps, 'onChange'> {
+  tooltip?: SliderTooltip;
+  prefixCls?: string;
+  className?: string;
+  onChange?(value: number[]): void;
+}
+
+export const Slider: React.FC<SliderProps> = ({
+  tooltip = {
+    placement: 'top',
+  },
+  min = 0,
+  max = 100,
+  value: sliderValues = [],
+  onChange,
+  className,
+  prefixCls = PREFIX_CLASS,
+  ...rest
+}) => {
+  const renderThumb = (value?: number) => {
+    const sliderValue = Number(value);
+    return (
+      <RadixUISlider.Thumb className={`${prefixCls}-slider-thumb`}>
+        {!tooltip?.formatterLabel && !!tooltip?.formatter && (
+          <div
+            className={clsx(`${prefixCls}-tooltip`, {
+              [`${prefixCls}-tooltip-placement-${tooltip?.placement}`]: !!tooltip?.placement,
+            })}
+          >
+            {tooltip?.formatter(sliderValue)}
+
+            <div className={`${prefixCls}-tooltip-arrow`} />
+          </div>
+        )}
+        {!!tooltip?.formatterLabel && !tooltip?.formatter && (
+          <div
+            style={{
+              left: sliderValue === max ? '-20px' : sliderValue === min ? '0px' : '-5px',
+            }}
+            className={clsx(`${prefixCls}-tooltip-label`, {
+              [`${prefixCls}-tooltip-placement-${tooltip?.placement}`]: !!tooltip?.placement,
+            })}
+          >
+            {tooltip?.formatterLabel(sliderValue)}
+          </div>
+        )}
+      </RadixUISlider.Thumb>
+    );
+  };
+
+  return (
+    <RadixUISlider.Root
+      {...rest}
+      min={min}
+      max={max}
+      value={sliderValues}
+      onValueChange={onChange}
+      className={clsx(`${prefixCls}-slider`, className)}
+    >
+      <RadixUISlider.Track className={`${prefixCls}-slider-track`}>
+        <RadixUISlider.Range className={`${prefixCls}-slider-range`} />
+      </RadixUISlider.Track>
+
+      {sliderValues.map((v, index) => (
+        <Fragment key={index}>{renderThumb(v)}</Fragment>
+      ))}
+    </RadixUISlider.Root>
+  );
 };
--- stories/components/Slider/Slider.stories.tsx
+++ stories/components/Slider/Slider.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 
 import { Slider } from '@/components';
+import { useState } from 'react';
 
 const meta = {
   title: 'In-Progress/Slider',
@@ -10,25 +10,116 @@ const meta = {
     layout: 'centered',
     chromatic: { disableSnapshot: false },
   },
-
-  tags: ['autodocs'],
-
   argTypes: {
-    size: {
-      control: 'inline-radio',
-      options: ['sm', 'md', 'lg'],
-      description: 'Set the size of tag',
+    min: { control: { type: 'number' }, description: 'Minimum value of the slider.' },
+    max: { control: { type: 'number' }, description: 'Maximum value of the slider.' },
+    value: {
+      control: { type: 'object' },
+      description: 'Current value(s) of the slider as an array.',
+    },
+    tooltip: {
+      control: { type: 'object' },
+      description: 'Tooltip settings (placement, formatter, formatterLabel).',
+    },
+    onChange: { action: 'changed', description: 'Callback when the slider value changes.' },
+    className: { control: { type: 'text' }, description: 'Custom class for styling.' },
+    prefixCls: {
+      control: { type: 'text' },
+      description: 'CSS prefix class for custom styles.',
+      table: {
+        disable: true,
+      },
     },
   },
-  // args: { onClick: fn() },
 } satisfies Meta<typeof Slider>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
+  args: {},
+  render: () => {
+    const [value, setValue] = useState([25]);
+
+    return (
+      <div className="my-10">
+        <Slider value={value} onChange={setValue} />
+      </div>
+    );
+  },
+};
+
+export const Range: Story = {
   args: {
-    size: 'md',
-    label: 'Holicay',
+    max: 100,
+    step: 5,
+  },
+  render: (args) => {
+    const [value, setValue] = useState([25, 50]);
+
+    return (
+      <div className="my-10">
+        <Slider {...args} value={value} onChange={setValue} />
+      </div>
+    );
+  },
+};
+
+export const RangeWithLabel: Story = {
+  args: {
+    max: 100,
+    step: 5,
+  },
+  render: (args) => {
+    const [value, setValue] = useState([25, 75]);
+
+    const formatterLabel = (value: number) => `${value}%`;
+
+    return (
+      <div className="my-10">
+        <div className="flex flex-col gap-y-6">
+          <Slider
+            {...args}
+            tooltip={{ placement: 'top', formatterLabel }}
+            value={value}
+            onChange={setValue}
+          />
+
+          <Slider
+            {...args}
+            tooltip={{ placement: 'bottom', formatterLabel }}
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+    );
+  },
+};
+
+export const RangeWithTooltip: Story = {
+  args: {
+    max: 100,
+    step: 5,
+  },
+  render: (args) => {
+    const [value, setValue] = useState([50, 100]);
+
+    const formatter = (value: number) => `${value}%`;
+
+    return (
+      <div className="my-10">
+        <div className="flex flex-col gap-y-6">
+          <Slider {...args} tooltip={{ formatter }} value={value} onChange={setValue} />
+
+          <Slider
+            {...args}
+            tooltip={{ formatter, placement: 'bottom' }}
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+    );
   },
 };
